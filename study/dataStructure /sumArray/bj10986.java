7c62b0ea4b66141c0de10c8e7df78478175c1007
Kim Daul <elre519@naver.com>
2026-01-11 21:05:02 +0900

    3일+디렉토리 수정

--- study/dataStructure /sumArray/bj10986.java
+++ study/dataStructure /sumArray/bj10986.java
@@ -1,3 +1,5 @@
+package sumArray;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
--- study/dataStructure /twoPointer/bj1940.java
+++ study/dataStructure /twoPointer/bj1940.java
@@ -1,3 +1,5 @@
+package twoPointer;
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
